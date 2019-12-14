db403f46eedb2f79a4b2b1e70b6461c35ed06b62
Jacek Ciesielski <ciesielskijacek@interia.pl>
2019-12-14 22:51:46 +0100

    test

---
+++ Test/Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Analyze
+    {
+        public void Run(string aPath)
+        {
+            List<string> filenames= new List<string>();
+            MakeAnalyze(aPath, filenames);
+
+
+
+
+
+
+        }
+        public void MakeAnalyze<string>(string aPath, List<string> aFileNames)
+        {
+            string[] directories = System.IO.Directory.GetDirectories(aPath);
+            foreach (var x in directories)
+            {
+                MakeAnalyze(x);
+
+            }
+
+
+        }
+    }
+
+
+
+}
